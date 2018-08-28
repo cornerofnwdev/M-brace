67ccb079b8b995d4efe6e9d36ddfc2bdd6d7fd0b
Nate Walker <Nate@Nathans-MacBook-Air.local>
2018-08-28 15:20:25 -0400

    First commit

---
+++ js/controllers/mainController.js
@@ -0,0 +1,24 @@
+app.controller('mainController', ['$scope', 
+	function($scope){
+		$scope.bannercity = 'img/bannercity.svg';
+		$scope.subtitle = 'M-Brace';
+		$scope.stadium = 'img/stadium.svg';
+		$scope.stadium2 = 'img/stadium2.svg';
+		$scope.nija = 'img/nwhite.jpg';
+		$scope.mike = 'img/mcarey.jpg';
+		$scope.brace = 'img/brace.png';
+		$scope.brace2 = 'img/brace2.png';
+		$scope.logo = 'img/logo.svg';
+		$scope.doctor = 'img/doctor.svg';
+		$scope.cityscape = 'img/cityscape.svg';
+		$scope.cityscape2 = 'img/cityscape2.svg';
+		$scope.who = 'Founded by two Bioengineering students one night in a library, we have humble beginnings with lofty goals.  Our flagship knee stabilizing unit will reduce the incidence of ACL injury and serve as a rehabilitation mechanism for athletes.';
+		$scope.who2 = 'The sports rehabilitation market was valued at $35 billion with an expected increase of 3.6 % by 2020. Our team is determined to create a customer-friendly devices that will bring a positive impact to the world of sports';
+		$scope.device = 'is a neurostimulation knee sleeve that utilizes a combination of electrical stimulation and nerve rehabilitation.';
+		$scope.device2 = 'The electrical stimulation helps to strengthen the muscles surrounding the ACL , while the nerve rehabilitation improves neuromuscular coordination. This device will be cost-effective, and will allow an athlete to return to playing capacity faster than ever following surgery.';
+		$scope.competition = 'current effective technologies and repurposes these tools to prevent future moments of knee collapse.  Our competitors such as Donjoy and McDavid solely provide stabilization, where Mbrace provides rehabilitation in addition to stability. Our devices also competes with current neuromuscular stimulators within the medical device market.';
+		$scope.competition2 = 'Mbrace surpasses these units through the incorporation of wearable technology.  MBrace allows for freedom of movement which contributes to the nerve rehabilitation and muscle contraction. In addition to these functional advances, our product prevents knee collapse for cheap.'
+		$scope.market = 'MBrace will be available for retail at $ 650/ unit and $850 per unit subscription and active servicing.';
+		$scope.market2 = 'In 2016, the sports medicine market was valued at $35 billion with an expected increase of 3.6% by 2020. Knee-based applications are expected to dominate the segment over this forecast period with the largest share of the market.';
+		$scope.market3 = 'ACL ligament repair accounted for the largest revenue at $1.9 billion as well as being the fastest growing segment. We plan on disrupting the current market with our device that will drastically outperform the competition.';
+}]);
